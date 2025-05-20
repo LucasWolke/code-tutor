cdb0fde6caf6e7d93a01d64f1503111a0d8ab394
Lucas Wolkersdorfer <e11922587@student.tuwien.ac.at>
2025-05-20 23:10:30 +0200

    update cors website link, add missing package

--- backend/sandbox/node-server/index.js
+++ backend/sandbox/node-server/index.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://code-tutor.vercel.app']
+    origin: ['http://localhost:3000', 'https://code-tutor-gray.vercel.app']
 }));
 app.use(express.json({ limit: '100kb' }));
 
